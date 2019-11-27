3971e63761644addde7a810e1bbd9d797171ba5e
joeycluett <joesunglassescool@gmail.com>
2019-11-26 22:43:53 -0600

    added vehicle to Bullet demo

--- bullet-vehicle/main.cpp
+++ bullet-vehicle/main.cpp
@@ -27,24 +27,37 @@ const int TEXTUREWIDTH  = 8;
 // place to store collision entity information
 btAlignedObjectArray<btCollisionShape*> collision_shapes;
 vector<btRigidBody*> rigid_body_list; // this holds info we want every frame
-vector<glm::mat4> transform_list;
+vector<glm::mat4> cube_transform_list;
+vector<glm::mat4> wheel_transform_list;
+vector<glm::mat4> x_wheel_transform_list;
+vector<glm::mat4> z_wheel_transform_list;
 
 void evaluateTransforms(void) {
-    transform_list.clear();
-    transform_list.resize(rigid_body_list.size());
+    cube_transform_list.clear();
+    wheel_transform_list.clear();
+    x_wheel_transform_list.clear();
+    z_wheel_transform_list.clear();
 
     size_t sz = rigid_body_list.size();
     for(unsigned int i = 0; i < sz; i++) {
+
+        btRigidBody* rb = rigid_body_list[i];
+        vector<glm::mat4>* ptr = (vector<glm::mat4>*)rb->getUserPointer();
+        ptr->push_back(glm::mat4());
+
         btTransform tf;
-        rigid_body_list[i]->getMotionState()->getWorldTransform(tf);
-        tf.getOpenGLMatrix(glm::value_ptr(transform_list[i]));
+        rb->getMotionState()->getWorldTransform(tf);
+        tf.getOpenGLMatrix(glm::value_ptr( ptr->back() ));
+
     }
 
-    //transform_list.push_back(glm::translate(glm::vec3{ 10.0f, -4.0f, 10.0f }));
-
+    cube_transform_list.push_back(glm::translate(glm::vec3{ 10.0f, -4.0f, 10.0f }));
+    wheel_transform_list.push_back(glm::translate(glm::vec3{ 10.0f, -4.0f, 10.0f }));
+    x_wheel_transform_list.push_back(glm::translate(glm::vec3{ 10.0f, -4.0f, 10.0f }));
+    z_wheel_transform_list.push_back(glm::translate(glm::vec3{ 10.0f, -4.0f, 10.0f }));
 }
 
-void addCube(btDiscreteDynamicsWorld* dynamics_world, glm::vec3 pos, float _mass = 4.0) {
+btRigidBody* addCube(btDiscreteDynamicsWorld* dynamics_world, glm::vec3 pos, float _mass = 4.0) {
     btCollisionShape* box_shape = new btBoxShape(btVector3(0.5, 0.5, 0.5));
     collision_shapes.push_back(box_shape);
 
@@ -57,17 +70,73 @@ void addCube(btDiscreteDynamicsWorld* dynamics_world, glm::vec3 pos, float _mass
     box_shape->calculateLocalInertia(m, local_inertia);
     btDefaultMotionState* motion_state = new btDefaultMotionState(box_tf);
     btRigidBody::btRigidBodyConstructionInfo rbInfo(m, motion_state, box_shape, local_inertia);
+
     btRigidBody* body = new btRigidBody(rbInfo);
+    body->setUserPointer((void*)&cube_transform_list);
     body->setFriction(0.9);
     dynamics_world->addRigidBody(body);
     rigid_body_list.push_back(body);
+    return body;
 }
+
+
+btRigidBody* addCylinder(btDiscreteDynamicsWorld* dynamics_world, glm::vec3 pos, char axis = 'y', float _mass = 4.0) {
+    btCollisionShape* cyl_shape = NULL;
+
+
+    if(axis == 'z') {
+        // rotate about x axis
+        cyl_shape = new btCylinderShapeZ(btVector3( 0.5, 0.5, 0.1 ));
+    }
+    else if(axis == 'x') {
+        // rotate about z axis
+        cyl_shape = new btCylinderShapeX(btVector3( 0.1, 0.5, 0.5 ));
+    }
+    else if(axis == 'y') {
+
+        cyl_shape = new btCylinderShape(btVector3( 0.5, 0.1, 0.5 ));
+    }
+
+    collision_shapes.push_back(cyl_shape);
+
+    btTransform wheel_tf;
+    wheel_tf.setIdentity();
+    wheel_tf.setOrigin(btVector3( 0.0, 0.0, 0.0 ));
+    wheel_tf.setOrigin(btVector3( pos.x, pos.y, pos.z ));
+
+    btScalar m(_mass);
+    btVector3 local_inertia(0.0, 0.0, 0.0);
+    cyl_shape->calculateLocalInertia(m, local_inertia);
+    btDefaultMotionState* motion_state = new btDefaultMotionState(wheel_tf);
+    btRigidBody::btRigidBodyConstructionInfo rbInfo(m, motion_state, cyl_shape, local_inertia);
+
+    btRigidBody* body = new btRigidBody(rbInfo);
+    if(axis == 'x')
+        body->setUserPointer((void*)&x_wheel_transform_list);
+    else if(axis == 'y')
+        body->setUserPointer((void*)&wheel_transform_list);
+    else if(axis == 'z')
+        body->setUserPointer((void*)&z_wheel_transform_list);
+
+    body->setFriction(0.9);
+    body->setRollingFriction(0.0);
+    dynamics_world->addRigidBody(body);
+    rigid_body_list.push_back(body);
+    return body;
+}
+
 
 struct {
     bool _up;
     bool _down;
     bool _left;
     bool _right;
+
+    bool _accel;
+    bool _decel;
+    bool _turn_left;
+    bool _turn_right;
+
 } button_state;
 
 auto key_callback(GLFWwindow* win, int key, int scancode, int action, int mods) -> void {
@@ -89,21 +158,15 @@ void evaluate_keyboard_input(GLFWwindow* win) {
     if(glfwGetKey(win, GLFW_KEY_ESCAPE) == GLFW_PRESS)
         glfwSetWindowShouldClose(win, true);
 
-    button_state._up =
-            glfwGetKey(win, GLFW_KEY_W) == GLFW_PRESS ||
-            glfwGetKey(win, GLFW_KEY_UP) == GLFW_PRESS;
+    button_state._up = glfwGetKey(win, GLFW_KEY_W) == GLFW_PRESS;
+    button_state._down = glfwGetKey(win, GLFW_KEY_S) == GLFW_PRESS;
+    button_state._left = glfwGetKey(win, GLFW_KEY_A) == GLFW_PRESS;
+    button_state._right = glfwGetKey(win, GLFW_KEY_D) == GLFW_PRESS;
 
-    button_state._down =
-            glfwGetKey(win, GLFW_KEY_S) == GLFW_PRESS ||
-            glfwGetKey(win, GLFW_KEY_DOWN) == GLFW_PRESS;
-
-    button_state._left =
-            glfwGetKey(win, GLFW_KEY_A) == GLFW_PRESS ||
-            glfwGetKey(win, GLFW_KEY_LEFT) == GLFW_PRESS;
-
-    button_state._right =
-            glfwGetKey(win, GLFW_KEY_D) == GLFW_PRESS ||
-            glfwGetKey(win, GLFW_KEY_RIGHT) == GLFW_PRESS;
+    button_state._accel = glfwGetKey(win, GLFW_KEY_UP) == GLFW_PRESS;
+    button_state._decel = glfwGetKey(win, GLFW_KEY_DOWN) == GLFW_PRESS;
+    button_state._turn_left = glfwGetKey(win, GLFW_KEY_LEFT) == GLFW_PRESS;
+    button_state._turn_right = glfwGetKey(win, GLFW_KEY_RIGHT) == GLFW_PRESS;
 
 }
 
@@ -112,7 +175,7 @@ int main(int argc, char* argv[]) {
     cout << "Creating window...\n" << flush;
     auto window = GLFWINITWINDOW(800, 600, "Hello World", {3, 3}, 4, true);
     glfwSetInputMode(window, GLFW_CURSOR, GLFW_CURSOR_DISABLED);
-    glfwSetInputMode(window, GLFW_STICKY_KEYS, GL_FALSE);
+    glfwSetInputMode(window, GLFW_STICKY_KEYS, GL_TRUE);
     cout << "DONE\n" << flush;
     cout << "Hello ReactPhysics3D\n";
 
@@ -141,30 +204,69 @@ int main(int argc, char* argv[]) {
         // specify where the ground is in world coordinates
         btTransform ground_tf;
         ground_tf.setIdentity();
-        ground_tf.setOrigin(btVector3( 25.0, -25.0, 25.0 ));
+        ground_tf.setOrigin(btVector3( 25.0, -25.0, 24.5 ));
 
         btScalar  m(0.0);
         btVector3 local_inertia(0.0, 0.0, 0.0);
         btDefaultMotionState* motion_state = new btDefaultMotionState(ground_tf);
         btRigidBody::btRigidBodyConstructionInfo rbInfo(m, motion_state, ground_shape, local_inertia);
         btRigidBody* body = new btRigidBody(rbInfo);
-        body->setFriction(1.0);
+        body->setFriction(0.9);
+        body->setRollingFriction(0.9);
 
         dynamicsWorld->addRigidBody(body);
     }
 
-    for(int i = 0; i < 40; i++) {
+    btRigidBody* rbA = addCube(dynamicsWorld, { 25.0, 10.0, 25.0 });
+    btRigidBody* rbB = addCube(dynamicsWorld, { 25.0, 12.0, 25.0 });
+    btRigidBody* rbC = addCube(dynamicsWorld, { 25.0, 14.0, 25.0 });
+    btRigidBody* rbD = addCube(dynamicsWorld, { 25.0, 16.0, 25.0 });
+    auto* p2p_0 = new btPoint2PointConstraint(*rbA, *rbB, btVector3(0.0, 1.0, 0.0), btVector3(0.0, -1.0, 0.0));
+    auto* p2p_1 = new btPoint2PointConstraint(*rbB, *rbC, btVector3(0.0, 1.0, 0.0), btVector3(0.0, -1.0, 0.0));
+    auto* p2p_2 = new btPoint2PointConstraint(*rbC, *rbD, btVector3(0.0, 1.0, 0.0), btVector3(0.0, -1.0, 0.0));
+    dynamicsWorld->addConstraint(p2p_0);
+    dynamicsWorld->addConstraint(p2p_1);
+    dynamicsWorld->addConstraint(p2p_2);
 
-        float y = 1.1*i + 0.5;
+    addCylinder(dynamicsWorld, {25.0, 25.0, 25.0}, 'x');
+    addCylinder(dynamicsWorld, {25.0, 27.0, 25.0}, 'z');
+    addCylinder(dynamicsWorld, {25.0, 29.0, 25.0});
 
-        //addCube(dynamicsWorld, {25.0, 0.5, 25.0}, 80.0);
 
-        addCube(dynamicsWorld, {25.0, y, 10.0 + (0.1*i)}, 0.1);
-        //addCube(dynamicsWorld, {23.0, y, 10.0 + (0.1*i)}, 80.0);
-        //addCube(dynamicsWorld, {21.0, y, 10.0 + (0.1*i)}, 80.0);
-        addCube(dynamicsWorld, {19.0, y, 10.0 + (0.1*i)}, 0.1);
+    // add all the entities needed for a small vehicle
+    btRigidBody* rbChassis        = addCube(dynamicsWorld,     { 10.0, 23.0, 10.0 }, 2.0);
+    btRigidBody* rbLeftWheel      = addCylinder(dynamicsWorld, { 11.0, 22.0, 12.0 }, 'x', 1.0);
+    btRigidBody* rbRightWheel     = addCylinder(dynamicsWorld, { 9.0,  22.0, 12.0 }, 'x', 1.0);
+    btRigidBody* rbRearLeftWheel  = addCylinder(dynamicsWorld, { 11.0, 22.0, 8.0 },  'x', 1.0);
+    btRigidBody* rbRearRightWheel = addCylinder(dynamicsWorld, { 9.0,  22.0, 8.0 },  'x', 1.0);
+    auto* hinge_0 = new btHingeConstraint(
+            *rbChassis, *rbLeftWheel,
+            btVector3(0.0, -1.0, 2.0), btVector3(-1.0, 0.0, 0.0),
+            btVector3(1.0, 0.0, 0.0),  btVector3(1.0, 0.0, 0.0));
+    auto* hinge_1 = new btHingeConstraint(
+            *rbChassis, *rbRightWheel,
+            btVector3(0.0, -1.0, 2.0), btVector3(1.0, 0.0, 0.0),
+            btVector3(1.0, 0.0, 0.0),  btVector3(1.0, 0.0, 0.0));
+    auto* hinge_2 = new btHingeConstraint(
+            *rbChassis, *rbRearLeftWheel,
+            btVector3(0.0, -1.0, -2.0), btVector3(-1.0, 0.0, 0.0),
+            btVector3(1.0, 0.0, 0.0),   btVector3(1.0, 0.0, 0.0));
+    auto* hinge_3 = new btHingeConstraint(
+            *rbChassis, *rbRearRightWheel,
+            btVector3(0.0, -1.0, -2.0), btVector3(1.0, 0.0, 0.0),
+            btVector3(1.0, 0.0, 0.0),   btVector3(1.0, 0.0, 0.0));
 
-    }
+    hinge_0->enableMotor(true);
+    hinge_1->enableMotor(true);
+    hinge_2->enableMotor(true);
+    hinge_3->enableMotor(true);
+
+    dynamicsWorld->addConstraint(hinge_0);
+    dynamicsWorld->addConstraint(hinge_1);
+    dynamicsWorld->addConstraint(hinge_2);
+    dynamicsWorld->addConstraint(hinge_3);
+
+    //addCube(dynamicsWorld, { 11.0, 0.6, 12.6 });
 
     button_state._up    = false;
     button_state._down  = false;
@@ -187,6 +289,9 @@ int main(int argc, char* argv[]) {
     Shader* ground_tex_shader;
     Shader* box_shader;
     ModelInfo box;
+    ModelInfo wheel;
+    ModelInfo x_wheel;
+    ModelInfo z_wheel;
     ModelInfo ground;
     ModelInfo ground_uv;
 
@@ -249,7 +354,10 @@ int main(int argc, char* argv[]) {
     {
         SimpleModelParser::setFileLocation("../assets/models/");
         SimpleModelParser::loadModelList({
-            { "box.txt", "box.box", &box }
+            { "box.txt",   "box.box",        &box },
+            { "wheel.txt", "toplevel.wheel", &wheel },
+            { "wheel.txt", "toplevel.x_wheel", &x_wheel },
+            { "wheel.txt", "toplevel.z_wheel", &z_wheel },
         });
 
         box_shader = new Shader( "instance" );
@@ -302,11 +410,55 @@ int main(int argc, char* argv[]) {
         auto new_mvp = Projection * camera.getTf() * Model;
         accumulated_time += delta_time;
 
+        float FORWARD = -12.0;
+
+        if(button_state._accel) {
+            hinge_0->enableAngularMotor(true, FORWARD, 6.0); // front left
+            hinge_1->enableAngularMotor(true, FORWARD, 6.0); // front right
+            hinge_2->enableAngularMotor(true, FORWARD, 6.0); // rear left
+            hinge_3->enableAngularMotor(true, FORWARD, 6.0); // rear right
+
+            if(button_state._turn_left) {
+                hinge_0->enableAngularMotor(true, 0, 6.0);
+                hinge_2->enableAngularMotor(true, 0, 6.0);
+            }
+            if(button_state._turn_right) {
+                hinge_1->enableAngularMotor(true, 0, 6.0);
+                hinge_3->enableAngularMotor(true, 0, 6.0);
+            }
+
+        }
+        else if(button_state._decel) {
+            hinge_0->enableAngularMotor(true, -FORWARD, 6.0);
+            hinge_1->enableAngularMotor(true, -FORWARD, 6.0);
+            hinge_2->enableAngularMotor(true, -FORWARD, 6.0);
+            hinge_3->enableAngularMotor(true, -FORWARD, 6.0);
+        }
+        else if(button_state._turn_left) {
+            hinge_0->enableAngularMotor(true, -FORWARD, 6.0);
+            hinge_1->enableAngularMotor(true, FORWARD, 6.0);
+            hinge_2->enableAngularMotor(true, -FORWARD, 6.0);
+            hinge_3->enableAngularMotor(true, FORWARD, 6.0);
+        }
+        else if(button_state._turn_right) {
+            hinge_0->enableAngularMotor(true, FORWARD, 6.0);
+            hinge_1->enableAngularMotor(true, -FORWARD, 6.0);
+            hinge_2->enableAngularMotor(true, FORWARD, 6.0);
+            hinge_3->enableAngularMotor(true, -FORWARD, 6.0);
+        }
+        else {
+            hinge_0->enableAngularMotor(true, 0.0, 6.0);
+            hinge_1->enableAngularMotor(true, 0.0, 6.0);
+            hinge_2->enableAngularMotor(true, 0.0, 6.0);
+            hinge_3->enableAngularMotor(true, 0.0, 6.0);
+        }
+
         // perform the physics simulation
         {
+
             const double timestep = 1.0/120.0;
             while(accumulated_time >= timestep) {
-                dynamicsWorld->stepSimulation(timestep);
+                dynamicsWorld->stepSimulation(timestep/1.0);
                 accumulated_time -= timestep;
             }
         }
@@ -321,14 +473,44 @@ int main(int argc, char* argv[]) {
 
             glBindBuffer(GL_ARRAY_BUFFER, box.buffer_id);
             box_shader->updateUniformData(color_index, (void*)&color);
-
-            for(auto& m : transform_list) {
+            for(auto& m : cube_transform_list) {
                 box_shader->use();
                 box_shader->updateUniformData(instance_index, (void*)glm::value_ptr(m));
                 glEnableVertexAttribArray(0);
                 glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 0, (void*)0);
                 glDrawArrays(GL_TRIANGLES, 0, box.vertices);
             }
+
+            glBindBuffer(GL_ARRAY_BUFFER, wheel.buffer_id);
+            box_shader->updateUniformData(color_index, (void*)&color);
+            for(auto& tf : wheel_transform_list) {
+                box_shader->use();
+                box_shader->updateUniformData(instance_index, (void*)glm::value_ptr(tf));
+                glEnableVertexAttribArray(0);
+                glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 0, (void*)0);
+                glDrawArrays(GL_TRIANGLES, 0, wheel.vertices);
+            }
+
+            glBindBuffer(GL_ARRAY_BUFFER, x_wheel.buffer_id);
+            box_shader->updateUniformData(color_index, (void*)&color);
+            for(auto& tf : x_wheel_transform_list) {
+                box_shader->use();
+                box_shader->updateUniformData(instance_index, (void*)glm::value_ptr(tf));
+                glEnableVertexAttribArray(0);
+                glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 0, (void*)0);
+                glDrawArrays(GL_TRIANGLES, 0, x_wheel.vertices);
+            }
+
+            glBindBuffer(GL_ARRAY_BUFFER, z_wheel.buffer_id);
+            box_shader->updateUniformData(color_index, (void*)&color);
+            for(auto& tf : z_wheel_transform_list) {
+                box_shader->use();
+                box_shader->updateUniformData(instance_index, (void*)glm::value_ptr(tf));
+                glEnableVertexAttribArray(0);
+                glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 0, (void*)0);
+                glDrawArrays(GL_TRIANGLES, 0, z_wheel.vertices);
+            }
+
         }
 
         // render the ground texture
